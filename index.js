2b184e5418ea7b2892c139efd0fe1fb08666d928
2betop <liaoxuezhi@icloud.com>
2015-05-28 21:46:22 +0800

    adjust to new api

--- index.js
+++ index.js
@@ -1,36 +1,75 @@
 /**
  * fis.baidu.com
  */
+var _ = fis.util;
 
-function upload(receiver, to, release, content, file, callback){
-    var subpath = file.subpath;
-    fis.util.upload(
-        //url, request options, post data, file
-        receiver, null, { to : to + release }, content, subpath,
-        function(err, res){
-            if(err || res.trim() != '0'){
-                fis.log.error('upload file [' + subpath + '] to [' + to +
-                    '] by receiver [' + receiver + '] error [' + (err || res) + ']');
-            } else {
-                var time = '[' + fis.log.now(true) + ']';
-                process.stdout.write(
-                    ' - '.green.bold +
-                    time.grey + ' ' + 
-                    subpath.replace(/^\//, '') +
-                    ' >> '.yellow.bold +
-                    to + release +
-                    '\n'
-                );
-                callback();
-            }
-        }
-    );
+function upload(receiver, to, release, content, file, callback) {
+  var subpath = file.subpath;
+  fis.util.upload(
+    //url, request options, post data, file
+    receiver, null, {
+      to: to + release
+    }, content, subpath,
+    function(err, res) {
+      if (err || res.trim() != '0') {
+        callback('upload file [' + subpath + '] to [' + to +
+          '] by receiver [' + receiver + '] error [' + (err || res) + ']');
+      } else {
+        var time = '[' + fis.log.now(true) + ']';
+        process.stdout.write(
+          ' - '.green.bold +
+          time.grey + ' ' +
+          subpath.replace(/^\//, '') +
+          ' >> '.yellow.bold +
+          to + release +
+          '\n'
+        );
+        callback();
+      }
+    }
+  );
 }
 
-module.exports = function (dest, file, content, settings, callback) {
-    var root = fis.project.getProjectPath();
-    var to = fis.util(dest.to);
-    if(settings && settings.receiver) {
-        upload(settings.receiver, to, dest.release, content, file, callback);
-    }
+module.exports = function(options, modified, total, callback) {
+  if (!options.to) {
+    throw new Error('options.to is required!');
+  } else if (!options.receiver) {
+    throw new Error('options.receiver is required!');
+  }
+
+  var to = options.to;
+  var receiver = options.receiver;
+
+  var steps = [];
+
+  modified.forEach(function(file) {
+    var reTryCount = options.retry;
+
+    steps.push(function(next) {
+      var _upload = arguments.callee;
+
+      upload(receiver, to, file.getHashRelease(), file.getContent(), file, function(error) {
+        if (error) {
+          if (!--reTryCount) {
+            throw new Error(error);
+          } else {
+            _upload();
+          }
+        } else {
+          next();
+        }
+      });
+    });
+  });
+
+  _.reduceRight(steps, function(next, current) {
+    return function() {
+      current(next);
+    };
+  }, callback)();
+};
+
+module.exports.options = {
+  // 允许重试两次。
+  retry: 2
 };
