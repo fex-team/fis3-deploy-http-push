dbf25c8e3000ec643727524e5eb6b8e06800c305
maxming <yonghua.myh@alibaba-inc.com>
2016-07-07 14:10:07 +0800

    增加 自定义 data 字段 功能

--- index.js
+++ index.js
@@ -3,30 +3,28 @@
  */
 var _ = fis.util;
 
-function upload(receiver, to, release, content, file, callback) {
+function upload(receiver, to, data, release, content, file, callback) {
   var subpath = file.subpath;
+  data['to'] = encodeURIComponent(to + release);
   fis.util.upload(
-    //url, request options, post data, file
-    receiver, null, {
-      to: encodeURIComponent(to + release)
-    }, content, encodeURIComponent(subpath),
-    function(err, res) {
-      if (err || res.trim() != '0') {
-        callback('upload file [' + subpath + '] to [' + to +
-          '] by receiver [' + receiver + '] error [' + (err || res) + ']');
-      } else {
-        var time = '[' + fis.log.now(true) + ']';
-        process.stdout.write(
-          ' - '.green.bold +
-          time.grey + ' ' +
-          subpath.replace(/^\//, '') +
-          ' >> '.yellow.bold +
-          to + release +
-          '\n'
-        );
-        callback();
+      //url, request options, post data, file
+      receiver, null, data, content, encodeURIComponent(subpath),
+      function(err, res) {
+        if (err || res.trim() != '0') {
+          callback('upload file [' + subpath + '] to [' + to + '] by receiver [' + receiver + '] error [' + (err || res) + ']');
+        } else {
+          var time = '[' + fis.log.now(true) + ']';
+          process.stdout.write(
+              ' - '.green.bold +
+              time.grey + ' ' +
+              subpath.replace(/^\//, '') +
+              ' >> '.yellow.bold +
+              to + release +
+              '\n'
+          );
+          callback();
+        }
       }
-    }
   );
 }
 
@@ -39,6 +37,7 @@ module.exports = function(options, modified, total, callback) {
 
   var to = options.to;
   var receiver = options.receiver;
+  var data = options.data || {};
 
   var steps = [];
 
@@ -48,7 +47,7 @@ module.exports = function(options, modified, total, callback) {
     steps.push(function(next) {
       var _upload = arguments.callee;
 
-      upload(receiver, to, file.getHashRelease(), file.getContent(), file, function(error) {
+      upload(receiver, to, data, file.getHashRelease(), file.getContent(), file, function(error) {
         if (error) {
           if (!--reTryCount) {
             throw new Error(error);
